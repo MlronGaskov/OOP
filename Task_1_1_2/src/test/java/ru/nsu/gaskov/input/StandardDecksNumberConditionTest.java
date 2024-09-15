dbc8ac776c62aa89367e810679ecdcc41ed5c4ef
MlronGaskov <144592511+MlronGaskov@users.noreply.github.com>
2024-09-15 22:20:48 +0700

    added tests

--- Task_1_1_2/src/test/java/ru/nsu/gaskov/input/StandardDecksNumberConditionTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/gaskov/input/StandardDecksNumberConditionTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StandardDecksNumberConditionTest {
-    /**@Test
+    @Test
     public void validDecksNumberTest() {
-        PlayersNumberCondition condition = new PlayersNumberCondition();
+        StandardDecksNumberCondition condition = new StandardDecksNumberCondition();
         assertTrue(condition.isValid("2"));
         assertTrue(condition.isValid("4"));
         assertTrue(condition.isValid("8"));
@@ -16,9 +16,9 @@ class StandardDecksNumberConditionTest {
 
     @Test
     public void invalidDecksNumberTest() {
-        PlayersNumberCondition condition = new PlayersNumberCondition();
+        StandardDecksNumberCondition condition = new StandardDecksNumberCondition();
         assertFalse(condition.isValid("-2"));
         assertFalse(condition.isValid("3412"));
         assertFalse(condition.isValid("miron2005"));
-    }*/
+    }
 }

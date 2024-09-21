9422d95973717b549da46585001f8ad06819c1d4
MlronGaskov <MlronGaskov@users.noreply.github.com>
2024-09-21 08:32:37 +0000

    Deploying to gh-pages from @ MlronGaskov/OOP@dba5ac93bd57c4418d80a9671f3d072aaac96fbd 🚀

--- Task_1_1_2/package-search-index.js
+++ Task_1_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.gaskov"},{"l":"ru.nsu.gaskov.actions"},{"l":"ru.nsu.gaskov.card"},{"l":"ru.nsu.gaskov.deck"},{"l":"ru.nsu.gaskov.input"},{"l":"ru.nsu.gaskov.player"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.gaskov.core"},{"l":"ru.nsu.gaskov.ui"}];updateSearchResults();

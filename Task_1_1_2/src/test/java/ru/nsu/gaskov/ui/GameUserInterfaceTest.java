778347b8c8337a56c71a1a1297d7ad07e0e6b6f1
MlronGaskov <144592511+MlronGaskov@users.noreply.github.com>
2024-09-21 20:56:36 +0700

    test

--- Task_1_1_2/src/test/java/ru/nsu/gaskov/ui/GameUserInterfaceTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/gaskov/ui/GameUserInterfaceTest.java
@@ -33,20 +33,16 @@ class GameUserInterfaceTest {
 
     @Test
     public void testGameEnd() {
-        InputStream inputStream = System.in;
-        PrintStream outputStream = System.out;
-
         String input = "1\nmiron\n1\n100\nno\nno\nend";
         ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        InputStream inputStream = System.in;
         System.setIn(in);
         final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+        PrintStream outputStream = System.out;
         System.setOut(new PrintStream(myOut));
-
         GameUserInterface.main(null);
-
         System.setIn(inputStream);
         System.setOut(outputStream);
-
         System.out.println(myOut.toString());
     }
 }

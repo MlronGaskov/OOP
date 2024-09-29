e66f262948a52043d134ddfe4f1d8e3a9c06da40
Miron Gaskov <m.gaskov@g.nsu.ru>
2024-09-29 21:21:07 +0700

    first commit

--- Task_1_1_3/src/test/java/ru/nsu/gaskov/ExpressionTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/gaskov/ExpressionTest.java
@@ -1,8 +1,10 @@
 package ru.nsu.gaskov;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 /**
  * Test.

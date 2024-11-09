68a6408a020c85bc8d2ee3b82b14ead69326e9f2
MlronGaskov <MlronGaskov@users.noreply.github.com>
2024-11-09 07:20:59 +0000

    Deploying to gh-pages from @ MlronGaskov/OOP@cef40e74b4e8bb2d36c3b336d189d690cb67b91b 🚀

--- Task_1_3_1/type-search-index.js
+++ Task_1_3_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.gaskov.text.search","l":"SubstringIndexIterator"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.gaskov.text.search","l":"KmpSubstringSearcher"}];updateSearchResults();

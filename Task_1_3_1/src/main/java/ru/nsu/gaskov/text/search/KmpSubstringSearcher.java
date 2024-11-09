29810f06faefd32a654aa820f33c446d2e2c731c
Miron Gaskov <m.gaskov@g.nsu.ru>
2024-11-09 14:27:49 +0700

    KmpSubstringSearcher

--- Task_1_3_1/src/main/java/ru/nsu/gaskov/text/search/KmpSubstringSearcher.java
+++ Task_1_3_1/src/main/java/ru/nsu/gaskov/text/search/KmpSubstringSearcher.java
@@ -8,8 +8,8 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Spliterators;
 import java.util.NoSuchElementException;
+import java.util.Spliterators;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 

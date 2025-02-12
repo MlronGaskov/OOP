4bf37645c1105627dfe55a681df2b4800d82f820
MlronGaskov <MlronGaskov@users.noreply.github.com>
2025-02-12 03:05:18 +0000

    Deploying to gh-pages from @ MlronGaskov/OOP@36f25bdb2f9b3640ad850dc91ae8d7f088462803 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.gaskov","l":"ParallelStreamInspector"},{"p":"ru.nsu.gaskov","l":"ParallelThreadsInspector"},{"p":"ru.nsu.gaskov","l":"PrimeChecker"},{"p":"ru.nsu.gaskov","l":"PrimeInspector"},{"p":"ru.nsu.gaskov","l":"SequentialInspector"}];updateSearchResults();

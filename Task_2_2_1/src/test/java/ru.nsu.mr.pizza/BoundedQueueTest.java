0033dac6917ae9f2be42d8a3c9f380aeeca3d8b6
Miron Gaskov <m.gaskov@g.nsu.ru>
2025-03-05 23:12:48 +0700

    style

--- Task_2_2_1/src/test/java/ru.nsu.mr.pizza/BoundedQueueTest.java
+++ Task_2_2_1/src/test/java/ru.nsu.mr.pizza/BoundedQueueTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -12,6 +11,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import org.junit.jupiter.api.Test;
 
 /**
  * Tests.

a7e51d943d3339f26b1f1bd1591e028f1809fdcc
Miron Gaskov <m.gaskov@g.nsu.ru>
2025-05-27 22:18:49 +0700

    code style

--- Task_2_1_2/src/test/java/ru/nsu/gaskov/prime/inspector/SocketUtilsTest.java
+++ Task_2_1_2/src/test/java/ru/nsu/gaskov/prime/inspector/SocketUtilsTest.java
@@ -6,11 +6,11 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.net.DatagramPacket;
 import java.net.MulticastSocket;
 import java.net.ServerSocket;
